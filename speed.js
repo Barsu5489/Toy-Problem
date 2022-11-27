9e521e9577f10922b9366d0c1887b2e01d0c3e0b
barsu5489 <ebarsulai@gmail.com>
2022-11-28 00:49:52 +0300

    speed detector

--- speed.js
+++ speed.js
@@ -6,19 +6,23 @@
 
 function carSpeed(speed)
 {
-    if (speed < 70)
+    if (speed < 70) // Output is 'OK' if speed is less than 70km/hr
     {
         return 'OK';
     }
-    else if (speed > 70 && (speed - 70) % 5 == 0 && (speed - 70) / 5 <= 12)
+    else if (speed > 70 && (speed - 70) % 5 == 0 && (speed - 70) / 5 <= 12) // calculates demerit points and outputs demerits points if it is less thsn 12 .
     {
         const points =  (speed - 70) / 5
         return `Points: `+ points
     }
-    else if ( (speed - 70) / 5 > 12 )
+    else if ( (speed - 70) / 5 > 12) // Checks if demerits points in more than 12 and outputs 'License suspended' if true.
     {
         return 'License suspended'
     }
+    else if (speed > 70 && (speed - 70) % 5 != 0) // Handle whole numbers not divisible by 5.
+    {
+        return 'Points will increase after 5km/hr speed increase'
+    }
     
 }
-console.log(carSpeed(133))
+console.log(carSpeed(100)) // Testing if program works 
