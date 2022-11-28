6b67275b902df8946686f893e2f2781f691e1a1f
barsu5489 <ebarsulai@gmail.com>
2022-11-28 09:15:46 +0300

    added a promp user interface to grade task

--- grade.js
+++ grade.js
@@ -4,31 +4,33 @@
 * return - Student grade depending on marks they got
 */
 
-function grade(marks)
+function grade()
 {
+    const marks = document.getElementById('input').value
+    const results = document.getElementById('grade')
+    
     if (marks > 79 && marks <= 100) // Grades between 79 - 100 output is grade A
     {
-        return 'Grade: A';
+        results.innerHTML =  'Grade: A';
     }
     else if (marks >= 60 && marks <= 79) // Grades between ^0 - 79 output is grade B
     {
-        return 'Grade: B';
+        results.innerHTML =  'Grade: B';
     }
     else if (marks >= 49 && marks <= 59) // Grades between 49 - 59 output is grade C
     {
-        return 'Grade: c';
+        results.innerHTML =  'Grade: c';
     }
     else if (marks >= 40 && marks <= 49 ) // Grades between 40 - 49 output is grade D
     {
-        return 'Grade: D';
+        results.innerHTML =  'Grade: D';
     }
     else if (marks >=0 && marks <40) // Grades between 0 - 40 output is grade A
     {
-        return 'Grade: E';
+        results.innerHTML =  'Grade: E';
     }
     else // if input is not between 0 - 100 warning is outputted
     {
-        return 'Try numbers between 0 - 100';
+        results.innerHTML =  'Try numbers between 0 - 100';
     }
 }
-console.log(grade(""))
